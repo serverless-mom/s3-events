0348c18324f8e6ac33175b9ec09cc634f9c43e85
tobyfee@stackery.io <tobyfee@stackery.io>
2018-04-19 20:10:04 +0000

    Modified 9 nodes:
    Added function node myLogger
    Added objectStore node 489d2685
    Deleted http node 86591158
    Deleted restApiEndpoint node 8b4a8772
    Deleted restApiEndpoint node 10125414
    Deleted function node Hello!
    Deleted function node Echo
    Deleted function node LogErrors
    Deleted errors node 342f3dda

--- Stackery/functions/LogErrors/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function handler(message, context, callback) {
-    console.log(`Error in node ${message.stackery.node.name} - ${message.class} - ${message.message} - stack trace: ${message.stack}`);
-    callback (null, {});
-}
