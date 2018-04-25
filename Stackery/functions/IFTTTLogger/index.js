e0c40b63fe98fad11713bf6d708c4beb978dc1a1
tobyfee@stackery.io <tobyfee@stackery.io>
2018-04-25 22:23:00 +0000

    Modified 1 node:
    Updated function node IFTTTLogger

--- Stackery/functions/IFTTTLogger/index.js
+++ Stackery/functions/IFTTTLogger/index.js
@@ -1,34 +1,12 @@
-var https = require('https');
-var querystring = require("querystring");
+let AWS = require('aws-sdk');
+let lambda = new AWS.Lambda();
 
-// IFTTT Maker Configuration, see https://ifttt.com/maker for more info
-var iftttMakerEventName = 'stackery_notification' // You can use a different IFTTT Maker EventName here
-var iftttMakerSecretKey = '<YOUR IFTTT MAKER SECRET KEY>';
-
-var iftttMakerUrl =
-  'https://maker.ifttt.com/trigger/' + iftttMakerEventName + '/with/key/' +
-  iftttMakerSecretKey;
-
-exports.handler = async message  => {
-  var messageString =
-    `Error in node ${message.stackery.node.name} - ${message.class} - ${message.message} - stack trace: ${message.stack}`
-  console.log('From Stackery:', messageString);
-
-
-  // The output is send as 'value1' to IFTTT Maker
-  var params = querystring.stringify({
-    value1: messageString
-  });
-
-  https.get(encodeURI(iftttMakerUrl) + '?' + params, function(res) {
-    console.log("Got response: " + res.statusCode);
-    res.setEncoding('utf8');
-    res.on('data', function(d) {
-      console.log('Body: ' + d);
-    });
-    console.log(res.statusCode);
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-    console.log(e.message);
-  });
+exports.handler = (event) => {
+    return new Promise((resolve, reject) => {
+        lambda.getAccountSettings(event)
+        .then((data) => {
+            resolve data;
+        })
+        .catch(reject);
+     });
 };
