229e425692ed008588df06d93ede734c64dd11ee
tobyfee@stackery.io <tobyfee@stackery.io>
2018-04-25 22:12:30 +0000

    Modified 1 node:
    Updated function node IFTTTLogger

--- Stackery/functions/IFTTTLogger/index.js
+++ Stackery/functions/IFTTTLogger/index.js
@@ -3,32 +3,32 @@ var querystring = require("querystring");
 
 // IFTTT Maker Configuration, see https://ifttt.com/maker for more info
 var iftttMakerEventName = 'stackery_notification' // You can use a different IFTTT Maker EventName here
-var iftttMakerSecretKey = 'cvVRFMOjjEMRo4kpa_wlBm';
+var iftttMakerSecretKey = '<YOUR IFTTT MAKER SECRET KEY>';
 
 var iftttMakerUrl =
-    'https://maker.ifttt.com/trigger/'
-    + iftttMakerEventName
-    + '/with/key/'
-    + iftttMakerSecretKey;
+  'https://maker.ifttt.com/trigger/' + iftttMakerEventName + '/with/key/' +
+  iftttMakerSecretKey;
 
-exports.handler = async message => {
-    var messageString = `Error in node ${message.stackery.node.name} - ${message.class} - ${message.message} - stack trace: ${message.stack}`
-    console.log('From Stackery:', messageString);
+exports.handler = async message  => {
+  var messageString =
+    `Error in node ${message.stackery.node.name} - ${message.class} - ${message.message} - stack trace: ${message.stack}`
+  console.log('From Stackery:', messageString);
 
 
-    // The output is send as 'value1' to IFTTT Maker
-    var params = querystring.stringify({value1: messageString});
+  // The output is send as 'value1' to IFTTT Maker
+  var params = querystring.stringify({
+    value1: messageString
+  });
 
-    https.get(encodeURI(iftttMakerUrl) + '?' + params, function(res) {
-        console.log("Got response: " + res.statusCode);
-        res.setEncoding('utf8');
-        res.on('data', function(d) {
-            console.log('Body: ' + d);
-        });
-        context.succeed(res.statusCode);
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-        context.fail(e.message);
+  https.get(encodeURI(iftttMakerUrl) + '?' + params, function(res) {
+    console.log("Got response: " + res.statusCode);
+    res.setEncoding('utf8');
+    res.on('data', function(d) {
+      console.log('Body: ' + d);
     });
-
+    resolve(res.statusCode);
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    reject(e.message);
+  });
 };
