a41074ebd8dd5ddeb432ff278a63e6d1bafc2887
tobyfee@stackery.io <tobyfee@stackery.io>
2018-04-25 22:30:54 +0000

    Modified 1 node:
    Updated function node IFTTTLogger

--- Stackery/functions/IFTTTLogger/index.js
+++ Stackery/functions/IFTTTLogger/index.js
@@ -1,12 +1,6 @@
 let AWS = require('aws-sdk');
 let lambda = new AWS.Lambda();
 
-exports.handler = (event) => {
-    return new Promise((resolve, reject) => {
-        lambda.getAccountSettings(event)
-        .then((data) => {
-            resolve data;
-        })
-        .catch(reject);
-     });
+exports.handler = async (event) => {
+    return await lambda.getAccountSettings().promise() ;
 };
